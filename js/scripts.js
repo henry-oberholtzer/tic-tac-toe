eef8748d6d6cda33ed2630879fb0e3330f17c859
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-01 11:06:08 -0700

    moveInput WIP
    Co-authored-by: Noah Kise <noah@kisefamily.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,23 @@ function WinConditionBoard(row1, row2, row3, col1, col2, col3, diag1, diag2) {
 
 const newGame = new WinConditionBoard([null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null]);
 
-function moveInput(index, player) {
-    newGame.row1[index] = player;
+function moveInput(newGame, index, player) {
+    const game = newGame
+    const gridMap = {
+        0: [game.row1[0], game.col1[0], game.diag1[0]],
+        1: [game.row1[1], game.col2[0]],
+        2: [game.row1[2], game.col3[0], game.diag2[0]],
+        3: [game.row2[0], game.col1[1]],
+        4: [game.row2[1], game.col2[1], game.diag1[1], game.diag2[1]],
+        5: [game.row2[2], game.col3[1]],
+        6: [game.row3[0], game.col1[2], game.diag2[2]],
+        7: [game.row3[1], game.col2[2]],
+        8: [game.row3[2], game.col3[2], game.diag1[2]],
+    };
+    console.log(gridMap);
+    gridMap[index].forEach((spot) => {
+        spot.value = player;
+    });
+    console.log(game)
+    console.log(gridMap[index]);
 }
