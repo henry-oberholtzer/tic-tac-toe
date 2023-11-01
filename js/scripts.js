0ff60d58916c0c89e727f344204d8b6a7f3f00b8
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-01 11:30:08 -0700

    moveInput function passes all tests
    Co-authored-by: Noah Kise <noah@kisefamily.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,25 +11,29 @@ function WinConditionBoard(row1, row2, row3, col1, col2, col3, diag1, diag2) {
     this.diag2 = diag2;
 }
 
-const newGame = new WinConditionBoard([null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null]);
+let newGame = new WinConditionBoard([null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null]);
 
-function moveInput(newGame, index, player) {
-    const game = newGame
+function moveInput(game, index, player) {
+    const playTurn = game
+    // let game = newGame
     const gridMap = {
-        0: [game.row1[0], game.col1[0], game.diag1[0]],
-        1: [game.row1[1], game.col2[0]],
-        2: [game.row1[2], game.col3[0], game.diag2[0]],
-        3: [game.row2[0], game.col1[1]],
-        4: [game.row2[1], game.col2[1], game.diag1[1], game.diag2[1]],
-        5: [game.row2[2], game.col3[1]],
-        6: [game.row3[0], game.col1[2], game.diag2[2]],
-        7: [game.row3[1], game.col2[2]],
-        8: [game.row3[2], game.col3[2], game.diag1[2]],
+        0: [["row1", 0], ["col1", 0], ["diag1", 0]],
+        1: [["row1", 1],["col2", 0]],
+        2: [["row1", 2], ["col3", 0], ["diag2", 0]],
+        3: [["row2", 0], ["col1", 1]],
+        4: [["row2", 1], ["col2", 1], ["diag1", 1], ["diag2", 1]],
+        5: [["row2", 2], ["col3", 1]],
+        6: [["row3", 0], ["col1", 2], ["diag2", 2]],
+        7: [["row3", 1], ["col2", 2]],
+        8: [["row3", 2], ["col3", 2], ["diag1", 2]],
     };
-    console.log(gridMap);
-    gridMap[index].forEach((spot) => {
-        spot.value = player;
+
+    const playArray = gridMap[index]
+    playArray.forEach((array) => {
+        const colRowDiag = array[0]
+        const index = array[1]
+        playTurn[colRowDiag][index] = player;
     });
-    console.log(game)
-    console.log(gridMap[index]);
+    newGame = playTurn;
+    console.log(newGame);
 }
