9bb24001e13f9ab629e1fefbbf279800756a8ad8
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-01 10:25:48 -0700

    pass WinConditionBoard tests
    Co-authored-by: Noah Kise <noah@kisefamily.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 // Business Logic
 
-function toggle() {
-    return "X";
+function WinConditionBoard(row1, row2, row3, col1, col2, col3, diag1, diag2) {
+    this.row1 = row1;
+    this.row2 = row2;
+    this.row3 = row3;
+    this.col1 = col1;
+    this.col2 = col2;
+    this.col3 = col3;
+    this.diag1 = diag1;
+    this.diag2 = diag2;
 }
+
+const newGame = new WinConditionBoard([null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null], [null, null, null]);
